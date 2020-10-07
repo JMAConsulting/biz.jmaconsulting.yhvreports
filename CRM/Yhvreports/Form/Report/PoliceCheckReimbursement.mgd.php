0b6724893016d13cc5620fb4033504dac4bb4011
Monish Deb <deb.monish@gmail.com>
2020-10-07 17:48:23 +0530

    Add PoliceCheckReimbursement report

---
+++ CRM/Yhvreports/Form/Report/PoliceCheckReimbursement.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+return [
+  [
+    'name' => 'CRM_Yhvreports_Form_Report_PoliceCheckReimbursement',
+    'entity' => 'ReportTemplate',
+    'params' => [
+      'version' => 3,
+      'label' => 'PoliceCheckReimbursement',
+      'description' => 'PoliceCheckReimbursement (biz.jmaconsulting.yhvreports)',
+      'class_name' => 'CRM_Yhvreports_Form_Report_PoliceCheckReimbursement',
+      'report_url' => 'biz.jmaconsulting.yhvreports/policecheckreimbursement',
+      'component' => '',
+    ],
+  ],
+];
