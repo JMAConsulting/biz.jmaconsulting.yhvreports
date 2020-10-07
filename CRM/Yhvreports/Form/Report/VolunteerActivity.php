6364125391cab1db66478b09e5da2a273cb82248
Monish Deb <deb.monish@gmail.com>
2020-10-07 15:57:07 +0530

    QA fix

--- CRM/Yhvreports/Form/Report/VolunteerActivity.php
+++ CRM/Yhvreports/Form/Report/VolunteerActivity.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/yhvreports.variables.php';
 use CRM_Yhvreports_ExtensionUtil as E;
 
 class CRM_Yhvreports_Form_Report_VolunteerActivity extends CRM_Report_Form_ActivitySummary {
-  
+
   protected $_customGroupExtends = ['Activity'];
 
   public function __construct() {
@@ -20,19 +20,16 @@ class CRM_Yhvreports_Form_Report_VolunteerActivity extends CRM_Report_Form_Activ
     ];
     $volunteeringTableName = civicrm_api3('CustomGroup', 'getvalue', ['id' => VOLUNTEERING_CG, 'return' => 'table_name']);
     foreach($this->_columns[$volunteeringTableName]['fields'] as $column => $field) {
+      if ($column == WORK_HOURS_CF) {
+        $this->_columns[$volunteeringTableName]['fields'][$column]['dbAlias'] = sprintf("SUM(%s)", $field['name']);
+      }
       $this->_columns[$volunteeringTableName]['group_bys'][$field['name']] = [
         'title' => $field['title'],
         'dbAlias' => "value_volunteering_12_civireport.{$field['name']}",
-      ]; 
-    }
-    $volunteeWFTableName = civicrm_api3('CustomGroup', 'getvalue', ['id' => VOLUTEER_WORK_HOURS_CG, 'return' => 'table_name']);
-    foreach($this->_columns[$volunteeringTableName]['fields'] as $column => $field) {
-      if ($column == WORK_HOURS_CF) {
-        $this->_columns[$volunteeWFTableName]['fields'][$column]['dbAlias'] = sprintf("SUM(%s)", $field['name']);
-      }
+      ];
     }
   }
-  
+
   /**
    * Group the fields.
    *
@@ -72,7 +69,7 @@ class CRM_Yhvreports_Form_Report_VolunteerActivity extends CRM_Report_Form_Activ
       $groupBy = $this->_groupBy;
       $this->_groupBy = "GROUP BY " . implode(', ', $this->_groupBy);
       if (strstr($this->_groupBy, 'value_volunteering_12_civireport') && !strstr($this->_from, 'value_volunteering_12_civireport')) {
-        $this->_from .= " 
+        $this->_from .= "
         LEFT JOIN civicrm_value_volunteering_12 value_volunteering_12_civireport ON value_volunteering_12_civireport.entity_id = activity_civireport.id ";
       }
     }
@@ -83,8 +80,10 @@ class CRM_Yhvreports_Form_Report_VolunteerActivity extends CRM_Report_Form_Activ
     if ($includeSelectCol) {
       $this->_groupBy = CRM_Contact_BAO_Query::getGroupByFromSelectColumns($this->_selectClauses, $groupBy);
     }
+    $this->_rollup = " WITH ROLLUP";
+    $this->_groupBy .= " {$this->_rollup}";
   }
-  
+
   public function alterDisplay(&$rows) {
     parent::alterDisplay($rows);
     if (array_key_exists('civicrm_activity_id_count', $this->_columnHeaders)) {
