99bf5810b8a0066ad0c3d87ad3046684ea272375
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2021-03-08 18:49:15 +0530

    Fixed activity type

--- CRM/Yhvreports/Form/Report/PoliceCheckReimbursement.php
+++ CRM/Yhvreports/Form/Report/PoliceCheckReimbursement.php
@@ -145,7 +145,7 @@ class CRM_Yhvreports_Form_Report_PoliceCheckReimbursement extends CRM_Report_For
                 FROM civicrm_activity as police_check
                 LEFT JOIN civicrm_activity_contact target_activity
                        ON police_check.id = target_activity.activity_id AND
-                       target_activity.record_type_id = {$targetID} AND {$policeCheckStatusClause} AND police_check.activity_type_id = 62
+                       target_activity.record_type_id = {$targetID} AND {$policeCheckStatusClause} AND police_check.activity_type_id = 59
                  GROUP BY target_activity.contact_id
                  ) temp_police_check ON temp_police_check.contact_id = contact_civireport.id
              {$this->_aclFrom}
